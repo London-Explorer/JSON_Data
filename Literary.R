5b70d9ab10973336b9d991d60c2e9c3a716ee535
DragonflyStats <kobriendublin@gmail.com>
2022-07-04 15:47:21 +0100

    Create Literary.R

---
+++ Literary.R
@@ -0,0 +1,7 @@
+Literary <- list(
+#Charles Dickens House
+list(Name="Charles Dickens House",
+     Location=c(51.52791483010891, -0.062431101592409916),
+     Type="Literary Location",
+     Area="City of London")
+)
