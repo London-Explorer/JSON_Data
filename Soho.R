8c7c9aac112994f5e9caeb3dbe5aa00b8ff355d7
DragonflyStats <kobriendublin@gmail.com>
2022-07-05 10:52:09 +0100

    Update and rename london.json to Soho.R

---
+++ Soho.R
@@ -0,0 +1,19 @@
+list(
+name =  "French House",
+street =  "Dean Street",
+suburb =  "Soho",
+city =  "Westminster",
+county =  "Greater London",
+state =  "England",
+postcode =  "W1D 5BG",
+country =  "United Kingdom",
+country_code =  "gb",
+lon =  -0.13171440925613825,
+lat =  51.512739499999995,
+state_code =  "ENG",
+formatted =  "French House, Dean Street, Westminster, W1D 5BG, United Kingdom",
+address_line1 =  "French House",
+address_line2 =  "Dean Street, Westminster, W1D 5BG, United Kingdom",
+category =  "catering.pub",
+result_type =  "amenity"
+)
