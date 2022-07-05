105f6dbbcdd4dae5093d6fbcbabb32dd6d6a359b
DragonflyStats <kobriendublin@gmail.com>
2022-07-05 17:59:07 +0100

    Create Mayfair.R

---
+++ Mayfair.R
@@ -0,0 +1,35 @@
+# Mayfair.R
+
+Mayfair = list(
+###########################
+list(
+Name="Ziggy Stardust Album Cover",
+Description = "Location of David Bowie's \"The Rise and Fall of Ziggy Stardust and the Spiders from Mars\" album cover photo",
+Type = "Notable Location",
+District = "Mayfair",
+Latitude = 51.511097859396166,
+Longitude = -0.139727568527199
+),
+###########################
+list(
+Name="Dover Street",
+District = "Mayfair",
+Latitude = 51.508397176899116, 
+Longitude = -0.14216333108090876
+),
+##########################
+list(
+Name="Berkeley Square",
+District = "Mayfair",
+Type="Park or Green-space",
+Latitude = 51.509674255135636, 
+Longitude = -0.14588623722891825
+),
+##########################
+list(
+Name="The Cartoon Museum",
+Latitude = 51.51757881963667, 
+Longitude = -0.13856919724380576
+)
+##########################
+)
