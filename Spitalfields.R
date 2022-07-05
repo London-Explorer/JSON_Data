cf6413878a506268c50aec771da8fc2fb66fb4df
DragonflyStats <kobriendublin@gmail.com>
2022-07-05 10:48:38 +0100

    Create Spitalfields.R

---
+++ Spitalfields.R
@@ -0,0 +1,7 @@
+
+list(
+Name = "Matchstick Girls Commemoration",
+Longitude = 51.52027885576338, 
+Latitude = -0.07293471428522216
+Info = "5 July 1888, 3 match girls were fired at  Bryant & May match factory in London for giving information about working conditions. The other 672 employees went on strike, a landmark for women workers in Britain that led to the formation of a Matchgirls' Union."
+)
