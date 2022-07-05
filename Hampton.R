7a30a18decf446eb07fdb1b4c5df44621ea51ccb
DragonflyStats <kobriendublin@gmail.com>
2022-07-05 17:58:33 +0100

    Create Hampton.R

---
+++ Hampton.R
@@ -0,0 +1,14 @@
+list(
+
+Name="Dorich House Museum",
+
+Description = "Dorich House is the former studio home of the sculptor Dora Gordine and her husband the Hon. Richard Hare, a scholar of Russian art and literature. Now Grade II listed, the building was completed in 1936, to Gordine’s design, and is an exceptional example of a modern studio house created by and for a female artist. Following Gordine’s death in 1991 the house was acquired and renovated by Kingston University and is now open to the public as a fully accredited museum."
+
+Latitude = 51.433343002382685,  
+
+Longitude = -0.26305880156308104,
+
+Website = "www.dorichhousemuseum.org.uk"
+
+)
+
